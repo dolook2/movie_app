aaed58a192a2a6ad75e74c047c9003e0808d9066
dolook2 <dolook2@naver.com>
2020-10-16 23:01:15 +0900

    code modification

---
+++ build/precache-manifest.41cb93195acbc977c4a1b3118ed691e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce7d3433f5afe0d2c20f0ccf2083b2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c47c14572f51a2631a4",
+    "url": "/static/css/main.40b71f36.chunk.css"
+  },
+  {
+    "revision": "d1a7fe3c7d49abd6b1d4",
+    "url": "/static/js/2.ade7a98c.chunk.js"
+  },
+  {
+    "revision": "6c47c14572f51a2631a4",
+    "url": "/static/js/main.eb2d6abf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
