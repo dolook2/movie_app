ebb5185cfe46320bb0506260ce11b7681f046bce
dolook2 <dolook2@naver.com>
2020-10-16 23:14:38 +0900

    Updates

---
+++ precache-manifest.60f7e30ea4acfafaa691ed5e19475b8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8aa95291de214e6ddea4e47ff7912a",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "6c47c14572f51a2631a4",
+    "url": "/movie_app/static/css/main.40b71f36.chunk.css"
+  },
+  {
+    "revision": "d1a7fe3c7d49abd6b1d4",
+    "url": "/movie_app/static/js/2.ade7a98c.chunk.js"
+  },
+  {
+    "revision": "6c47c14572f51a2631a4",
+    "url": "/movie_app/static/js/main.eb2d6abf.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
